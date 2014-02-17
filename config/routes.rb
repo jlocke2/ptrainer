ac70a0d2fd63c0f6551693c157616398bb67deb7
Alan Locke <jlocke2288@gmail.com>
2014-02-17 14:19:59 -0500

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ptrainer::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
