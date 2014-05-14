54425c739d3a482c84b78fe9733cbda3757d3379
Alan Locke <jlocke2288@gmail.com>
2014-05-14 01:02:20 -0400

    Testing side

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -3,48 +3,11 @@ class HardWorker
 
   def perform
     
-  	require 'mandrill'
-  
-          mandrill = Mandrill::API.new 'gdATMo6lVK4YKoTdolhuBQ'
-          message = {"html"=>"#{render_to_string('workouts/workout_email', :layout => false)}",
-           "text"=>"Example text content",
-           "subject"=>"Upcoming Workout",
-           "from_email"=>"#{current_user.email}",
-           "from_name"=>"",
-           "to"=>
-              [{"email"=>"#{Workout.find(params[:id]).appointment.client.email}",
-                  "name"=>"#{Workout.find(params[:id]).appointment.client.name}",
-                  "type"=>"to"}],
-           "headers"=>{"Reply-To"=>"#{current_user.email}"},
-           "important"=>false,
-           "track_opens"=>nil,
-           "track_clicks"=>nil,
-           "auto_text"=>nil,
-           "auto_html"=>nil,
-           "inline_css"=>nil,
-           "url_strip_qs"=>nil,
-           "preserve_recipients"=>nil,
-           "view_content_link"=>nil,
-           "bcc_address"=>"",
-           "tracking_domain"=>nil,
-           "signing_domain"=>nil,
-           "return_path_domain"=>nil,}
-           
-          async = false
-          ip_pool = "Main Pool"
-          send_at = ""
-          result = mandrill.messages.send message, async, ip_pool, send_at
-              # [{"email"=>"recipient.email@example.com",
-              #     "status"=>"sent",
-              #     "reject_reason"=>"hard-bounce",
-              #     "_id"=>"abc123abc123abc123abc123abc123"}]
-
-
+Rails.logger.debug("debug::" + "yes")
           
       
       
 
-end
 
 
   end
