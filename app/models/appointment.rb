32fd3b3c154a4fa3bdec83632bf3e609381a3d1e
Alan Locke <jlocke2288@gmail.com>
2014-10-31 16:19:55 -0400

    Fix currency again

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -187,9 +187,9 @@ require 'mandrill'
         @rev = @times * @default_price
         @profit = @rev.to_f * 0.0967 - (55 * @times)
         @profit_final = @profit/100
-        @currency = number_to_currency(@profit_final)
+        @currency = @profit_final.to_s
         @opening = "Congratulations #{trainer.name} on your successful week!  Below we have included your weekly summary for your convenience.</p>"\
-        "<p>You were able to book a total of #{@times} sessions this week for a profit of #{}."
+        "<p>You were able to book a total of #{@times} sessions this week for a profit of $#{@currency}."
         
         
           if @unpaid.count > 0
